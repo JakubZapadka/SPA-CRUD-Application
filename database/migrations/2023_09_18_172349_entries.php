289d3d16fdb06976cc943f570b51eec1c485886c
JakubZapadka <Jakubzapadka1@gmail.com>
2023-09-18 19:27:39 +0200

    database migrations

--- database/migrations/2023_09_18_172349_entries.php
+++ database/migrations/2023_09_18_172349_entries.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('entries', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('surname');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('role');
+            $table->string('title');
+            $table->string('content');
+            $table->timestamps();
         });
     }
 
@@ -26,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('entries');
     }
 };
