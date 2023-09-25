c8a4a496e23d11fd7c130b1c2c66f6f18cd91dfe
Jakub Zapadka <jakubzapadka1@gmail.com>
2023-09-25 14:44:18 +0200

    Authentication layout

---
+++ resources/js/components/app/CreateNewDropdown.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        Dropdown
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ resources/js/components/app/Navigation.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav class="min-w-[200px]">
+        <div class="h-[80px] px-3 flex items-center gap-3">
+            <Link href="/">
+                <ApplicationLogo 
+                    class="block h-9 w-auto fill-current text-gray-800" 
+                />
+            </Link>
+            LaraCRUD
+        </div>
+        <div class="px-3">
+            <CreateNewDropdown />
+
+            <div class="py-3">
+                <NavLink href="/">My Profile</NavLink>
+                <NavLink href="/">Entries</NavLink>
+                <NavLink href="/">Users</NavLink>
+                <NavLink href="/" :active="true">Logout</NavLink>
+            </div>
+        </div>
+    </nav>
+</template>
+<script setup>
+import ApplicationLogo from '@/components/ApplicationLogo.vue';
+import { Link } from '@inertiajs/vue3';
+import CreateNewDropdown from '@/components/app/CreateNewDropdown.vue';
+import NavLink from '@/components/NavLink.vue';
+
+</script>
+<style scoped>
+</style>
