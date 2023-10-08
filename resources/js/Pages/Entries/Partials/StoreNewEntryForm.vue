e608a41a4bd8539fa76f91b28787245326e994a9
JakubZapadka <Jakubzapadka1@gmail.com>
2023-10-08 12:19:09 +0200

    z-index bug fix

--- resources/js/Pages/Entries/Partials/StoreNewEntryForm.vue
+++ resources/js/Pages/Entries/Partials/StoreNewEntryForm.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="z-10">
         <button @click="showForm = !showForm" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">New Item</button>
         <div v-if="showForm" class="fixed inset-0 flex items-center justify-center bg-black/25">
             <div class="bg-white p-4 rounded-lg sm:w-1/2 w-5/6">
